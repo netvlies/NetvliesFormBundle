f3e83927b180f7a6a1153b28d287196a2b8ffde1
Richard van den Brand <richard@netvlies.nl>
2012-01-16 14:13:20 +0100

    added min, max and stepping to vars

--- Form/Type/SliderType.php
+++ Form/Type/SliderType.php
@@ -7,10 +7,27 @@
 namespace Netvlies\Bundle\FormBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormBuilder;
 
 class SliderType extends AbstractType
 {
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder->setAttribute('min', $options['min']);
+        $builder->setAttribute('max', $options['max']);
+        $builder->setAttribute('stepping', $options['stepping']);
+    }
+
+    public function buildViewBottomUp(FormView $view, FormInterface $form)
+    {
+        $view->set('min', $form->getAttribute('min'));
+        $view->set('max', $form->getAttribute('max'));
+        $view->set('stepping', $form->getAttribute('stepping'));
+    }
+
+
     /**
      * {@inheritdoc}
      */
